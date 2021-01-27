9dd4a1141555107b77d36b2e8d418f43b928ea58
Sourabh Choudhary <sourabh.choudhary@stryker.com>
2021-01-27 21:09:03 +0530

    Added Unit Test project

---
+++ PromotionEngineApp/PromotionEngineAppTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PromotionEngineAppTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
