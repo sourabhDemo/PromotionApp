fcde3944ec1b6665d1549d176f70b284a7117c8a
Sourabh Choudhary <sourabh.choudhary@stryker.com>
2021-01-28 10:01:06 +0530

    Added support for console output from Main method

--- PromotionEngineApp/PromotionEngineApp/Program.cs
+++ PromotionEngineApp/PromotionEngineApp/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using PromotionEngineApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PromotionEngineApp
 {
@@ -6,7 +9,24 @@ namespace PromotionEngineApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Order firstOrder = new Order(1, new List<Sku>() { new Sku("A"), new Sku("B"), new Sku("C") });
+            Order secondOrder = new Order(2, new List<Sku>() { new Sku("A"), new Sku("A"), new Sku("A"), new Sku("A"), new Sku("A"), new Sku("B"), new Sku("B"), new Sku("B"), new Sku("B"), new Sku("B"), new Sku("C") });
+            Order thirdOrder = new Order(3, new List<Sku>() { new Sku("A"), new Sku("A"), new Sku("A"), new Sku("B"), new Sku("B"), new Sku("B"), new Sku("B"), new Sku("B"), new Sku("C"), new Sku("D") });
+
+            List<Order> orders = new List<Order>();
+            orders.AddRange(new Order[] { firstOrder, secondOrder, thirdOrder });
+            var activePromotion = new ActivePromotion();
+            foreach (Order order in orders)
+            {
+                var promotions = activePromotion.GetPromotions(order);
+                List<decimal> promotionPrices = promotions
+                    .Select(promotion => activePromotion.GetPromotionPrice(order, promotion))
+                    .ToList();
+                decimal totalPrice = order.StockUnits.Sum(x => x.Price);
+                decimal promotionPrice = promotionPrices.Sum();
+                Console.WriteLine($"OrderID: {order.OrderId}");
+                Console.WriteLine($"Final price: {(totalPrice - promotionPrice):0.00}");
+            }
         }
     }
 }
